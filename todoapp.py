80f7e136f9cf963f97e792e14d8198eef5ef411b
Owner <Owner@Owners-iMac-4.local>
2020-12-08 02:49:03 -0500

    initial commit

--- todoapp.py
+++ todoapp.py
@@ -1,35 +1,52 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.db'
 db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    priority = db.Column(db.String(20), nullable=False)
+    title = db.Column(db.String(), nullable=False)
+    author = db.Column(db.String(), nullable=False)
+    thumbnail = db.Column(db.String(), nullable=False)
+    page_count = db.Column(db.Integer, nullable=False)
+    avg_rating = db.Column(db.Float, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Todo %r>' % self.id
+        return '<Book %r>' % self.id
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(300), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
 
 
 @app.route('/', methods=['GET'])
 def display_todo_list():
-    todos = Todo.query.order_by(Todo.timestamp).all()
-    return render_template('index.html', todos=todos)
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 
-@app.route('/submit', methods=['POST'])
-def add_todo():
-    title = request.form.get('task')
+@app.route('/search', methods=['GET'])
+def index():
+    return render_template('search.html')
+
+
+@app.route('/post_user', methods=['POST'])
+def post_user():
     email = request.form.get('email')
-    priority = request.form.get('priority')
-    new_todo = Todo(text=title, email=email, priority=priority)
+    password = request.form.get('password')
+    new_todo = User(email=email, password=password)
     try:
         db.session.add(new_todo)
         db.session.commit()
@@ -38,30 +55,59 @@ def add_todo():
         return redirect('/')
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    todo_to_delete = Todo.query.get_or_404(id)
-
+@app.route('/login_user/')
+def login_user():
+    login_message = ''
+    email = request.args.get('email')
+    password = request.args.get('password')
+    user = User.query.filter_by(email=email).first()
     try:
-        db.session.delete(todo_to_delete)
-        db.session.commit()
-        return redirect('/')
+        if user and user.password == password:
+            return render_template('/booklists.html')
+        else:
+            login_message = "Incorrect Login Credentials!"
     except:
-        return redirect('/')
+        return render_template('login.html', login_message=login_message)
 
 
-@app.route('/clear')
-def clear():
-    todos = Todo.query.all()
+@app.route('/search_book/')
+def search_book():
+    search_message = ''
+    isbn = request.args.get('isbn')
+    url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:{}'.format(isbn)
     try:
-        for todo in todos:
-            todo_to_delete = Todo.query.get_or_404(todo.id)
-            db.session.delete(todo_to_delete)
-        db.session.commit()
-        return redirect('/')
+        response = requests.get(url)
+        return render_template('search.html', books=response.json())
     except:
-        return redirect('/')
+        return render_template('search.html')
 
+
+#
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     todo_to_delete = Todo.query.get_or_404(id)
+#
+#     try:
+#         db.session.delete(todo_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return redirect('/')
+#
+#
+# @app.route('/clear')
+# def clear():
+#     todos = Todo.query.all()
+#     try:
+#         for todo in todos:
+#             todo_to_delete = Todo.query.get_or_404(todo.id)
+#             db.session.delete(todo_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return redirect('/')
+#
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
